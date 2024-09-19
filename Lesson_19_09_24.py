fa106059b29a60373199c97ef8e3cd0a14fab24b
ZodiacLens <beanies.fabled_0f@icloud.com>
2024-09-19 23:06:40 +0200

    2.1 Converting a user's 4-digit number into a column. 2.2 "Reversing" a user's 5-digit number.

--- Lesson_19_09_24.py
+++ Lesson_19_09_24.py
@@ -1,3 +1,4 @@
+# Задание 2.1
 user_number = input("Введите 4-х значное число: ")
 dig_1 = int(user_number) // 1000
 dig_2 = (int(user_number) % 1000) // 100
@@ -7,3 +8,12 @@ print(dig_1)
 print(dig_2)
 print(dig_3)
 print(dig_4)
+
+# Задание 2.2
+user_number = input("Введите 5-ти значное число: ")
+dig_1 = int(user_number) % 10
+dig_2 = (int(user_number) % 100) // 10
+dig_3 = (int(user_number) % 1000) // 100
+dig_4 = (int(user_number) % 10000) // 1000
+dig_5 = (int(user_number) % 100000) // 10000
+print(dig_1, dig_2, dig_3, dig_4, dig_5)
