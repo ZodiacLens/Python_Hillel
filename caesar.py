a9f34aa1cbeb2f91280e5e1a32dc72168ec797eb
ZodiacLens <beanies.fabled_0f@icloud.com>
2024-10-03 09:13:39 +0200

    Homework from 30_09_24 Add. Encryptor.

--- caesar.py
+++ caesar.py
@@ -1,14 +1,14 @@
-def caesar (text, shift = -7 ):
-    result = ""
-    for symbol in text:
-        if symbol.isalpha():
-            base = ord('А') if symbol.isupper() else ord('а')
-            shifted_symbol = chr((ord(symbol) - base + shift) % 33 + base)
-            result += shifted_symbol
-        else:
-            result += symbol
-    return result
-
-original_text = "П цхрлъ е лхтпфхр шумчщфхр щмфп п фм ъихдшв отз.\nЩйхр цхшхь, щйхр нмот — хфп ъшцхсзпйздщ умфе."
-decrypted_text = caesar(original_text)
-print(decrypted_text)
+# def caesar (text, shift = -7 ):
+#     result = ""
+#     for symbol in text:
+#         if symbol.isalpha():
+#             base = ord('А') if symbol.isupper() else ord('а')
+#             shifted_symbol = chr((ord(symbol) - base + shift) % 33 + base)
+#             result += shifted_symbol
+#         else:
+#             result += symbol
+#     return result
+#
+# original_text = "П цхрлъ е лхтпфхр шумчщфхр щмфп п фм ъихдшв отз.\nЩйхр цхшхь, щйхр нмот — хфп ъшцхсзпйздщ умфе."
+# decrypted_text = caesar(original_text)
+# print(decrypted_text)
